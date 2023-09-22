deb59049520e0c331041bf56016328b5fe5b9420
amitt <amittomar991@gmail.com>
2023-09-22 18:50:47 +0530

    Initial commit-The initialization of project!..

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SearchAlgos searchAlgos = new SearchAlgos();
+        SortingAlgo sortingAlgo = new SortingAlgo();
+        Arrays arrays = new Arrays();
+        LinklistQues linklistQues = new LinklistQues();
+        StackQues stackQues = new StackQues();
+        QueueQues queueQues = new QueueQues();
+        GraphQues graphQues = new GraphQues();
+        TreeQues treeQues = new TreeQues();
+    }
+}
