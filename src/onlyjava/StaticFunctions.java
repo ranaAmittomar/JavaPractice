d888cf3e1bc9c57eff2179ea4ea6f6448192f59f
Amit Kumar Singh <aksofficial991@gmail.com>
2023-10-28 08:12:39 +0530

    Static Functions

--- src/onlyjava/StaticFunctions.java
+++ src/onlyjava/StaticFunctions.java
@@ -1,24 +1,31 @@
 package onlyjava;
 
 //we define non static functions and we can all them from other class.
+//We can make many methods private ,and then make one method to call from other class
+//to hide/abstract making our class.
 
 public class StaticFunctions {
-    int count; // Static name.
+
+    private int count; // Static name.
     // Below are non static functions which are being called inside MathsFuncs.java
     // class.
+    static final double RANDOM_NUM;
+    static {
+        RANDOM_NUM = (double) Math.random();
+    }
 
-    public int count() {
-        for (int i = 0; i < 6; i++) {
+    private int count() {
+        for (int i = 0; i < 2; i++) {
             count++;
         }
         return count;
     }
 
-    public int addNum(int n) {
-        return n + count();
+    private double addNum(int n) {
+        return n + RANDOM_NUM + count();
     }
 
-    public int getSum(int num) {
+    public double getSum(int num) { // CALLED IN MATHSFUNCS.JAVA class.
         return addNum(num);
     }
 
