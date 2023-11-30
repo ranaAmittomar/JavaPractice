1939ac825ecf2a609e2caa70c4b5eb1db98e20bb
Amit Kumar Singh <aksofficial991@gmail.com>
2023-11-30 13:12:56 +0530

    log funcs

--- src/onlyjava/MathsFuncs.java
+++ src/onlyjava/MathsFuncs.java
@@ -29,22 +29,32 @@ public class MathsFuncs {
         }
     }
 
+    public static void logFuncs() {
+        int n = 10;
+        System.out.println(Math.log(n));
+        System.out.println("Log base 10: " + Math.log10(n)); // base 10
+    }
+
     public static void main(String[] args) {
-        StaticFunctions staticFunctions = new StaticFunctions();
-        System.out.println(staticFunctions.getSum(22) + PI); // from static func class
-        StaticFunctions.maxAndmin();
-        int formatNum = 100000000;
-        int num1 = 1;
-        int num2 = 3;
-        for (int i : mathsReturn(num1, num2)) {
-            System.out.print(i + " ");
-        }
-        System.out.println("\n" + MathsFuncs.PI);
-        String s = String.format("%,d, ", formatNum); // String formatting.
-        System.out.println(s);
-        System.out.println(String.format("I have %,.2f bugs to fix", 32.556789));
-        System.out.println(String.format("I have %x hexadecimal to fix", 42));
-        System.out.println(String.format("I have '%c' characters to fix", 67));
-        System.out.println(divideNum(2, 0));
+        /*
+         * StaticFunctions staticFunctions = new StaticFunctions();
+         * System.out.println(staticFunctions.getSum(22) + PI); // from static func
+         * class
+         * StaticFunctions.maxAndmin();
+         * int formatNum = 100000000;
+         * int num1 = 1;
+         * int num2 = 3;
+         * for (int i : mathsReturn(num1, num2)) {
+         * System.out.print(i + " ");
+         * }
+         * System.out.println("\n" + MathsFuncs.PI);
+         * String s = String.format("%,d, ", formatNum); // String formatting.
+         * System.out.println(s);
+         * System.out.println(String.format("I have %,.2f bugs to fix", 32.556789));
+         * System.out.println(String.format("I have %x hexadecimal to fix", 42));
+         * System.out.println(String.format("I have '%c' characters to fix", 67));
+         * System.out.println(divideNum(2, 0));
+         */
+        logFuncs();
     }
 }
