09b64e77a03d414bcdb8c311b522bbe507cba901
Amit Kumar Singh <aksofficial991@gmail.com>
2023-12-03 05:47:04 +0530

    generating permutations

--- src/algorithmBased/BacktrackingQues.java
+++ src/algorithmBased/BacktrackingQues.java
@@ -3,18 +3,60 @@ package algorithmBased;
 public class BacktrackingQues {
 
     // including the questions based on advance recursion and backtracking.
+    // Q1 :- Generate permutations of array and using backtrack.
+    public static void generatePermutaions(int[] nums) {
+        backtracking(nums, 0);
 
-    private static boolean sudokuSolver() {
-        helperBoard();
-        return true;
     }
 
-    private static void helperBoard() {
-        
+    public static void backtracking(int[] nums, int start) {
+        int n = nums.length;
+        if (start == n) {
+            // permution found,
+            printArray(nums);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!containsDuplicate(nums, start, i)) {
+                swap(nums, start, i); // swap elements at index i and start.
+                backtracking(nums, start + 1); // recursive call for remaining elements.
+                // backtrack-undo the swap for next iteration.
+                swap(nums, start, i);
+            }
+
+        }
+    }
+
+    private static boolean containsDuplicate(int[] nums, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (nums[i] == nums[end]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+    }
+
+    private static void printArray(int[] nums) {
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        sudokuSolver();
+
+        int[] num = { 1, 2 };
+        generatePermutaions(num);
     }
 
 }
