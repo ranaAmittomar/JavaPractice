ac183c9e3612748dcafcda368dc14ece0dbb4b24
Amit Kumar Singh <aksofficial991@gmail.com>
2023-12-03 05:47:29 +0530

    docString and more methdos

--- src/dsa/Recursion.java
+++ src/dsa/Recursion.java
@@ -2,6 +2,115 @@ package dsa;
 
 import java.util.ArrayList;
 
+/**
+ * The `Recursion` class contains various recursive methods that demonstrate
+ * different concepts and techniques related to recursion.
+ * These methods include calculating the Fibonacci sequence, printing the sum of
+ * numbers, printing numbers in ascending and descending order,
+ * printing alphabets for numbers, calculating the power of a number, checking
+ * if an array is sorted, generating subsets of an array,
+ * generating subsets of a string, solving the Tower of Hanoi problem,
+ * generating permutations of a string, counting the number of jumps to reach a
+ * certain number,
+ * swapping characters in a string, checking if an array is sorted, and summing
+ * the elements of an array.
+ *
+ * Example Usage:
+ * // Calculate the Fibonacci sequence for n=10
+ * int fibResult = Recursion.fib(10);
+ * System.out.println(fibResult); // Output: 55
+ *
+ * // Print the sum of numbers from 1 to n=5
+ * int sumResult = Recursion.printingSum(5);
+ * System.out.println(sumResult); // Output: 15
+ *
+ * // Print numbers from 1 to n=5 in ascending order
+ * Recursion.printNums(5); // Output: 1 2 3 4 5
+ *
+ * // Print numbers from 1 to n=5 in descending order
+ * Recursion.printNumsRev(5); // Output: 5 4 3 2 1
+ *
+ * // Print alphabets for numbers from 1 to n=5
+ * String[] str = {"zero", "one", "two", "three", "four", "five"};
+ * Recursion.printAlphaNums(str, 5); // Output: five four three two one
+ *
+ * // Calculate the power of 2 for timesPow=3 and num=7
+ * int powResult = Recursion.powOf2(3, 7);
+ * System.out.println(powResult); // Output: 343
+ *
+ * // Calculate the fast exponential for time=3 and num=2
+ * int expoResult = Recursion.fastExpo(3, 2);
+ * System.out.println(expoResult); // Output: 8
+ *
+ * // Check if an array is sorted
+ * int[] arr = {1, 2, 3, 4, 5};
+ * boolean isSorted = Recursion.sortedArrCheck(arr, 1);
+ * System.out.println(isSorted); // Output: true
+ *
+ * // Generate subsets of an array
+ * ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
+ * ArrayList<Integer> output = new ArrayList<>();
+ * Recursion.subsetsArray(input, output, 0);
+ * // Output:
+ * // 123
+ * // 12
+ * // 13
+ * // 1
+ * // 23
+ * // 2
+ * // 3
+ *
+ * // Generate subsets of a string
+ * String inputString = "abc";
+ * String outputString = "";
+ * Recursion.subsetString(inputString, outputString, 0);
+ * // Output:
+ * // abc
+ * // ab
+ * // ac
+ * // a
+ * // bc
+ * // b
+ * // c
+ *
+ * // Calculate the number of jumps to reach a certain number
+ * int jumpResult = Recursion.jumpStairs(5);
+ * System.out.println(jumpResult); // Output: 13
+ *
+ * // Swap characters in a string and generate permutations
+ * String str = "abc";
+ * Recursion.permutationStr(str, 0);
+ * // Output:
+ * // abc
+ * // acb
+ * // bac
+ * // bca
+ * // cab
+ * // cba
+ *
+ * // Solve the Tower of Hanoi problem
+ * Recursion.towerOfHanoi(3, 'A', 'B', 'C');
+ * // Output:
+ * // Move disk 1 from rod A to rod C
+ * // Move disk 2 from rod A to rod B
+ * // Move disk 1 from rod C to rod B
+ * // Move disk 3 from rod A to rod C
+ * // Move disk 1 from rod B to rod A
+ * // Move disk 2 from rod B to rod C
+ * // Move disk 1 from rod A to rod C
+ *
+ * // Test a weird recursive function
+ * int weirdResult = Recursion.testingWeirdFunc(5);
+ * System.out.println(weirdResult); // Output: 120
+ *
+ * // Check if an array is sorted and calculate the sum of its elements
+ * int[] arr = {1, 2, 3};
+ * int size = arr.length;
+ * boolean isSorted = Recursion.isSortedArray(arr, size);
+ * System.out.println(isSorted); // Output: true
+ * int sumResult = Recursion.arraySum(arr, size);
+ * System.out.println(sumResult); // Output: 6
+ */
 public class Recursion {
 
     public static int fib(int n) {
@@ -154,13 +263,65 @@ public class Recursion {
     }
 
     public static int testingWeirdFunc(int n) {
+
+        // recursion rel in the end :- TAIL RECURSION
         System.out.println(n);
         if (n == 1) {
             return 1;
         }
         System.out.println(n);
-        return n*testingWeirdFunc(n-1);
+        return n * testingWeirdFunc(n - 1);
 
+    }
+
+    // checking if array is sorted..
+    public static boolean isSortedArray(int[] arr, int size) {
+
+        if (size == 0 || size == 1)
+            return true;
+        if (arr[0] > arr[1])
+            return false;
+        else {
+            // creating the subarray to pass.
+            int[] subArray = new int[size - 1];
+            System.arraycopy(arr, 1, subArray, 0, size - 1);
+            boolean rem = isSortedArray(subArray, size - 1);
+            return rem;
+        }
+    }
+
+    // summing the array.
+    public static int arraySum(int[] arr, int size) {
+        if (size == 0)
+            return 0;
+        if (size == 1)
+            return arr[0];
+        int[] subArray = new int[size - 1];
+        System.arraycopy(arr, 1, subArray, 0, size - 1);
+        int sum = arr[0] + arraySum(subArray, size - 1);
+        return sum;
+    }
+
+    public static boolean linearSearch(int[] arr, int size, int key) {
+        printArrayLinearSearch(arr, size);
+        if (size == 0)
+            return false;
+        if (arr[0] == key)
+            return true;
+        else {
+            int[] subArray = new int[size - 1];
+            System.arraycopy(arr, 1, subArray, 0, size - 1);
+            return linearSearch(subArray, size - 1, key);
+        }
+
+    }
+
+    private static void printArrayLinearSearch(int[] arr, int size) {
+        System.out.println("Size of array: " + size);
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -194,7 +355,13 @@ public class Recursion {
         // permutationStr(s, 0);
         // int n = 3;
         // towerOfHanoi(n,'A','B','C');
-        System.out.println(testingWeirdFunc(5));
+        // System.out.println(testingWeirdFunc(5));
+        int[] arr = { 1, 2, 3 }; // sorted array.
+        int size = arr.length;
+        int key = 3;
+        System.out.println(isSortedArray(arr, size));
+        System.out.println(arraySum(arr, size));
+        System.out.println(linearSearch(arr, size, key));
     }
 
 }
