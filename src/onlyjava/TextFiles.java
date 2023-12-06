7afb96ee1c7e9843aa4e0434a8830e85ca924136
Amit Kumar Singh <aksofficial991@gmail.com>
2023-12-07 03:30:49 +0530

    file I/O important

--- src/onlyjava/TextFiles.java
+++ src/onlyjava/TextFiles.java
@@ -1,44 +1,53 @@
 package onlyjava;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TextFiles {
 
-    ArrayList<String> songList = new ArrayList<String>();
+    public static void readingTheFile() {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("output.txt"))) {
+
+            System.out.println(reader.readLine()); // we're reading the first line from the file.
+
+            // to read every single line..follow the below loop.
+            String line; // it'll store the every line while we iterate over string in file.
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close(); // t's important and good practice to close the reader object.
+
+        } catch (IOException e) {
+
+            System.out.println("Cannot read the file content");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void writingTheFile() { // method to write inside a txt file.
+        String[] names = { "Amit", "Shweta", "Meena", "Tomar" };
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            writer.write("Writing from the java class"); // it'll write inside the txt file.
+            writer.write("\nThis is another line.");
+            writer.write("This is in the same line");
+            // for automatic addition.. we'll loop through names to add that in lines.
+            for (String name : names) {
+                writer.write("\n" + name);
+            }
+            writer.close(); // it's important and good practice to close the writer object.
+        } catch (IOException e) {
+            System.out.println("Unable to create the file.");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        new TextFiles().go();
-    }
-
-    public void go() {
-        getSongs();
-        System.out.println(songList);
-        Collections.sort(songList);
-        System.out.println(songList);
-    }
-
-    void getSongs() {
-        try {
-            File file = new File("SongList.txt"); // Specify the correct file path
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                addSong(line);
-            }
-            reader.close(); // Close the BufferedReader to release resources
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    void addSong(String lineToParse) {
-        String[] tokens = lineToParse.split("/"); // Use standard double quotes for splitting
-        if (tokens.length > 0) { // Check if there are tokens before accessing them
-            songList.add(tokens[0]);
-        }
+        writingTheFile();
+        readingTheFile();
     }
 }
