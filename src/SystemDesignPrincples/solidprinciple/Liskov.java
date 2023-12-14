6f00fc7bc11e63f45fc2ae2ab146c6198378d5c0
Amit Kumar Singh <aksofficial991@gmail.com>
2023-12-14 07:28:19 +0530

    Liskov.java Implementation

--- src/SystemDesignPrincples/solidprinciple/Liskov.java
+++ src/SystemDesignPrincples/solidprinciple/Liskov.java
@@ -41,9 +41,9 @@ class MotorCycle extends EngineVehicle {
 public class Liskov {
     public static void main(String[] args) {
         List<Vehicle> vehicleList = new ArrayList<>();
-        vehicleList.add(new Bicycle());
-        vehicleList.add(new Car());
-        vehicleList.add(new MotorCycle());
+        vehicleList.add(new Bicycle()); // object from bicycle class
+        vehicleList.add(new Car()); // object from car class
+        vehicleList.add(new MotorCycle()); // object from motorcycle class
         for (Vehicle vehicle : vehicleList) {
             System.out.println(vehicle.getNumberOfWheels().toString());
         }
