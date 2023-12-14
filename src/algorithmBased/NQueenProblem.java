981c8f23f860e64bb822e3d53e1de2bdea873965
Amit Kumar Singh <aksofficial991@gmail.com>
2023-12-14 08:31:49 +0530

    Yet To be Implemented

---
+++ src/algorithmBased/NQueenProblem.java
@@ -0,0 +1,7 @@
+package algorithmBased;
+
+//famous N-Queen Problem . 
+
+public class NQueenProblem {
+
+}
---
+++ src/algorithmBased/RatInMaze.java
@@ -0,0 +1,11 @@
+package algorithmBased;
+
+//famous rat in maze problem to reach from source to destination .
+//using recursion and backtracking.
+public class RatInMaze {
+
+    public static void main(String[] args) {
+
+    }
+
+}
